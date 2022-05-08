c23358966f17d9134bc19af850a644b3f2dc3aa5
kazisharifulislam <kazisharifulislam52@gmail.com>
2022-05-08 23:14:46 +0600

    environment variable

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,13 +6,20 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCa-hvKtN_LUiW7hI_CTz3O3qC_guC_uNY",
-  authDomain: "assignment-11-e988e.firebaseapp.com",
-  projectId: "assignment-11-e988e",
-  storageBucket: "assignment-11-e988e.appspot.com",
-  messagingSenderId: "125178063563",
-  appId: "1:125178063563:web:39aabc762209c6fe3ec94e"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId
 };
+
+/* apiKey: "AIzaSyCa-hvKtN_LUiW7hI_CTz3O3qC_guC_uNY",
+authDomain: "assignment-11-e988e.firebaseapp.com",
+projectId: "assignment-11-e988e",
+storageBucket: "assignment-11-e988e.appspot.com",
+messagingSenderId: "125178063563",
+appId: "1:125178063563:web:39aabc762209c6fe3ec94e", */
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- src/pages/home/Footer.js
+++ src/pages/home/Footer.js
@@ -3,60 +3,60 @@ import "./footer.css";
 
 const Footer = () => {
   return (
-    <footer class="bg-light text-center footer-shadow ">
-      <div class="container p-4 pb-0 ">
-        <section class="mb-4">
+    <footer className="bg-light text-center footer-shadow ">
+      <div className="container p-4 pb-0 ">
+        <section className="mb-4">
           <a
-            class="btn btn-primary btn-floating m-1"
+            className="btn btn-primary btn-floating m-1"
             style={{ backgroundColor: "#3b5998" }}
             href="#!"
           >
-            <i class="ri-facebook-fill"></i>
+            <i className="ri-facebook-fill"></i>
           </a>
 
           <a
-            class="btn btn-primary btn-floating m-1"
+            className="btn btn-primary btn-floating m-1"
             style={{ backgroundColor: "#55acee" }}
             href="#!"
           >
-            <i class="ri-twitter-fill"></i>
+            <i className="ri-twitter-fill"></i>
           </a>
 
           <a
-            class="btn btn-primary btn-floating m-1"
+            className="btn btn-primary btn-floating m-1"
             style={{ backgroundColor: "#dd4b39" }}
             href="#!"
           >
-            <i class="ri-google-fill"></i>
+            <i className="ri-google-fill"></i>
           </a>
 
           <a
-            class="btn btn-primary btn-floating m-1"
+            className="btn btn-primary btn-floating m-1"
             style={{ backgroundColor: "#ac2bac" }}
             href="#!"
           >
-            <i class="ri-instagram-fill"></i>
+            <i className="ri-instagram-fill"></i>
           </a>
 
           <a
-            class="btn btn-primary btn-floating m-1"
+            className="btn btn-primary btn-floating m-1"
             style={{ backgroundColor: "#0082ca" }}
             href="#!"
           >
-            <i class="ri-stack-overflow-fill"></i>
+            <i className="ri-stack-overflow-fill"></i>
           </a>
 
           <a
-            class="btn btn-primary btn-floating m-1"
+            className="btn btn-primary btn-floating m-1"
             style={{ backgroundColor: "#333333" }}
             href="#!"
           >
-            <i class="ri-github-fill"></i>
+            <i className="ri-github-fill"></i>
           </a>
         </section>
       </div>
 
-      <div class="text-center bg-dark p-3 text-white">
+      <div className="text-center bg-dark p-3 text-white">
         &copy; 2022 Copyright Phoenix Web
       </div>
     </footer>
